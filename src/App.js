8514eb14e2882b5e5fc7c436e37188db87cbba4b
Navruz Zubaydullayev <zubaydullayevnavruz@gmail.com>
2020-10-16 02:41:57 +0500

    body updated

--- src/App.js
+++ src/App.js
@@ -11,4 +11,8 @@ function App() {
 }
 
 
+
+
+
+
 export default App;
--- src/components/body/body.js
+++ src/components/body/body.js
@@ -4,7 +4,7 @@ class Body extends Component {
     render() {
         return (
             <div>
-                
+                qdqwdqwdqwdqw
             </div>
         );
     }
