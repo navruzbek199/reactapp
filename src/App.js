929a743101ffb842fb5dcde923aa2d9a4eb21124
Navruz Zubaydullayev <zubaydullayevnavruz@gmail.com>
2020-10-16 02:34:22 +0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Navbar from "./components/navbar/navbar";
+
+function App() {
+  return (
+    <div className="App">
+        <Navbar/>
+      Hello React Js
+    </div>
+  );
+}
+
+
+export default App;
